786356ba6ff1fc8b66ac523a9d4419c20f64289b
NinoSiegrist <39274907+NinoSiegrist@users.noreply.github.com>
2018-05-14 21:50:19 +0200

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,11 @@
+# Funktion:
+# modul wortzeichen_modul setz die umgewandelten Sonderzeichen wieder zurück
+# parameter:    trace_flag = "yes"  => aktivieren des traces
+#               word                    = zu verifizierendes wort
+#               sprache
+
+
+trace_flag      = "no"
+print ("   - start modul wortzeichen - trace_flag: ", trace_flag)
+
+
